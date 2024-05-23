2efa0d8c993a3c5aa31f02356b6f3f82047a19a7
Carl <carl.mikaelsson@chasacademy.se>
2024-05-23 14:41:16 +0200

    test

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const { json } = require("body-parser")
+const { create } = require("domain")
+const { response } = require("express")
+const http = require("http")
+
+const PORT = process.env.PORT || 3000
+
+const server = http.createServer((request, response) => {
+    if (request.method === "GET" && request.url == "/") {
+        response.writeHead(200, { "Content-Type": "application/json" })
+        response.write(JSON.stringify({ message: "Hello from server!" }))
+        response.end()
+        return
+    }
+    else {
+        response.writeHead(404, { "Content-Type": "application/json" })
+        response.end(JSON.stringify({ message: "Route not found" }))
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Server on http://localhost:${PORT}`)
+})
