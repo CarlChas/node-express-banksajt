d6892a23d744cb1b83fcee582c1a52290c713ec6
Carl <carl.mikaelsson@chasacademy.se>
2024-05-23 15:18:58 +0200

    j

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 
 // Middleware
 app.use(bodyParser.json())
-app.use(cors)
+app.use(cors())
 
 // Routes - Endpoints - Behöver url + http-metod för att triggas
 //Method GET
