99f34d2e78386c2fa5ea13596092f7c460ffb723
Carl <carl.mikaelsson@chasacademy.se>
2024-05-23 15:00:35 +0200

    j

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,25 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Routes - Endpoints - Behöver url + http-metod för att triggas
+//Method GET
 app.get("/", (req, res) => {
     //Det som triggas kallas för "routehandler"
     res.send("Hello from route '/'")
+})
+
+//Method POST
+app.post("/whatever", (req, res) => {
+    res.send("A POST-request")
+})
+
+//Method PUT
+app.put("/whatever", (req, res) => {
+    res.send("A PUT-request")
+})
+
+//Method DELETE
+app.delete("/whatever", (req, res) => {
+    res.send("A DELETE-request")
 })
 
 app.listen(PORT, () => {
